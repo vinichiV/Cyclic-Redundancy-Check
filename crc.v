cfbb3752544de700d6263750bd8e46d677cc7197
Vinh Dang <168114878+vinichiV@users.noreply.github.com>
2026-06-25 14:48:48 +0700

    Update crc.v

--- crc.v
+++ crc.v
@@ -15,33 +15,37 @@ module crc #(parameter width = 32, parameter poly_width = 9) // adjust
     if(reset) begin
       state <= 0;
     end
-  end
-  
-  always @(posedge clk) begin
-    case(state)
-      0:begin
-        i <= width;
-        shift_poly <= poly << (width - 1);
-        temp <= message << (poly_width - 1);
-        state <= 1;
-      end
-      1: begin
-        if(temp[width + poly_width - 2]) begin
-          temp <= (temp ^ shift_poly) << 1;
-        end else begin
-          temp <= temp << 1;
-        end
-        i = i - 1;
-        state <= 2;
-      end
-      2: begin
-        if(i == 0) begin
-          final_value <= temp[width + poly_width - 2:width];
-        end else state <= 1;
-      end
+    
+	  else begin
+		 case(state)
+			0:begin
+			  i <= width;
+			  shift_poly <= poly << (width - 1);
+			  temp <= message << (poly_width - 1);
+			  state <= 1;
+			end
+       
+			1: begin
+			  if(temp[width + poly_width - 2]) begin
+				 temp <= (temp ^ shift_poly) << 1;
+			  end else begin
+				 temp <= temp << 1;
+			  end
+			  i <= i - 1;
+			  state <= 2;
+			end
+       
+			2: begin
+			  if(i == 0) begin
+				 final_value <= temp[width + poly_width - 2:width];
+				 state <= 0;
+			  end else state <= 1;
+			end
     endcase
   end
+    
+end
   
-  assign crc_value = final_value[poly_width - 2:0];
+assign crc_value = final_value[poly_width - 2:0];
   
 endmodule
